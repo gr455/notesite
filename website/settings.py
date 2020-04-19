2ecec06318b1facab649cb23749e54b9876c9697
gr455 <ruturaj.mohite.iiitb.org>
2020-04-19 15:20:35 +0530

    debug false for deploy

--- website/settings.py
+++ website/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'atemporarysecretkeynotusedfordeployment'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
